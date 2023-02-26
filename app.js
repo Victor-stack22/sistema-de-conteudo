4e4173277b3742fe4d24bea1d194c655663c3915
unknown <victor.pintassilgo@gmail.com>
2023-02-26 10:04:59 -0300

    treinando branch e commit

--- app.js
+++ app.js
@@ -3,3 +3,6 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 //mais um console log de teste
 console.log("Enviando para o GitHub")
 console.log("Corrigindo o app js")
+
+//fazendo alterações
+console.log("treinando branch e commits")
